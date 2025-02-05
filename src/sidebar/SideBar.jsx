bb5234fb0589acedff131dbaeb287db1dfac1526
Densingh-123 <ddensingh19@gmail.com>
2025-02-05 17:01:19 +0530

    Update SideBar.jsx

--- src/sidebar/SideBar.jsx
+++ src/sidebar/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Sidebar.css';
+import './SideBar.css';
 import { FaTachometerAlt, FaUsers, FaDatabase, FaSignOutAlt, FaComments } from 'react-icons/fa';
 
 const Sidebar = () => {
